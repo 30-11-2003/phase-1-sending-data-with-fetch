0bb861aad136052ccb35646dfc68654b3891cafe
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2023-10-16 23:51:32 +0300

    Updated fetch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const users ={
+    usersName:"Henry Owen",
+    usersEmail:"Owenhenry@gmail.com",
+    id :5
+}
+function submitData(users){
+    
+    console.log (submitData(usersName,usersEmail))
+
+   fetch(' http://localhost:3000/users',{
+        method :"POST",
+        headers: {
+            "Content-Type":"application/json",
+            Accept: "application/json",
+        },
+        body :JSON.stringify(submitData)        
+        })
+    
+    .then (res=>res.json)
+    .then (users=> console.log (`users ${id}`))
+
+    .catch (function (error) {
+        alert ("There is an error");
+        console.log(error.message);
+    })
+}
